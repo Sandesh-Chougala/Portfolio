9439cbbeb00512af95dd55fd11bdf8b5837ec154
Sandesh Chougala <sandeshchougala205@gmail.com>
2025-09-09 12:24:18 +0530

    about page added

--- src/componets/About.js
+++ src/componets/About.js
@@ -6,12 +6,31 @@ class About extends React.Component {
     return (
       <div className="about" id="about">
         <div className="left-side">
-          <img src="./assets/icons/about.gif" alt="About" />
+          <img src=".\assets\icons\about.gif" alt="About" />
         </div>
         <div className="right-side">
           <div className="content">
             <h1 style={{ color: "rgba(141, 214, 31, 1)" }}>About</h1>
             <h1>Me</h1>
+          </div>
+          <p>
+            Hii My self, Sandesh I'm a FullStack Developer<br></br>
+            I'm From Belagavi,Karnataka,India <br></br>I Buid Modern,powefull
+            and Responsive Web Sites<br></br>Using
+            HTML,CSS,JS,React,Next.js,Python and Java
+          </p>
+          <div className="social">
+            <a
+              className="hk"
+              href="https://drive.google.com/file/d/1hRlhvb6oxc07bOxmiAiIesAJPlTaiQnw/view?usp=drive_link"
+            >
+              Resume
+              <img src="./assets/icons/redirect.png" alt="Redirect"></img>
+            </a>
+            <a className="hk">
+              Contact Me
+              <img src="./assets/icons/redirect.png" alt="Redirect"></img>
+            </a>
           </div>
         </div>
       </div>
